c6e4c9e61b1eb17f612e2e3a6b555738b4ed0899
Manfred Ogbu <bizzyman007@gmail.com>
2023-01-24 18:14:27 +0000

    Task 0 and README.md files done

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""empty class Square that defines a square"""
-
-
 class Square:
-    """empty class Square that defines a square"""
-        pass
+    """Class Square that defines a square.
+    """
+    pass
